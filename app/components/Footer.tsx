683594964934811da13aeafcfd0c0779d91577ba
Eniz <dajiceniz@gmail.com>
2026-07-30 12:54:19 +0200

    1254
    break

---
+++ app/components/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div className="wrap">
+        <div></div>
+        <div>
+          <p>Test3</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
