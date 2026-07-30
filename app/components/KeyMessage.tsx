4c644d5c647efcd14034bd10d57fa3aa80a9ab22
Eniz <dajiceniz@gmail.com>
2026-07-30 14:57:21 +0200

    www
    ,.

--- app/components/KeyMessage.tsx
+++ app/components/KeyMessage.tsx
@@ -1,8 +1,19 @@
 export default function KeyMessage() {
   return (
     <>
-      <section>
-        <div>KeyMessage</div>
+      <section className="pad">
+        <div className="wrap split">
+          <div className="reveal vis">
+            <span className="eyebrow">// Kernbotschaft</span>
+            <h2>
+              Software fürs
+              <b> Handwerk,</b>
+              <br></br>
+              die im Alltag funktioniert.
+            </h2>
+          </div>
+          <div></div>
+        </div>
       </section>
     </>
   );
